31a2e271ae8741f89790246f02350a4fbbc8711f
John Conover <111jbcon@gmail.com>
2016-05-10 11:30:45 -0400

    Added random sphere generation

--- raytrace_pthread.cpp
+++ raytrace_pthread.cpp
@@ -131,6 +131,13 @@ void *render_thread(void *threadarg){
 	pthread_exit(NULL);
 }
 
+void random_spheres(std::vector<Sphere> &spheres, int n){
+    for (int i = 0; i < n; i++){
+        spheres.push_back(Sphere(Vec3f( drand48() * 10 - 5, drand48() * 3 - 1, -drand48() * 40), drand48() * 1.2, Vec3f(drand48(), drand48(), drand48()), 0, drand48()));
+    }
+}
+
+
 int main(int argc, char **argv)
 {
 
@@ -143,11 +150,13 @@ int main(int argc, char **argv)
 	std::vector<Sphere> spheres;
 	// position, radius, surface color, reflectivity, transparency, emission color
 	spheres.push_back(Sphere(Vec3f( 0.0, -10004, -20), 10000, Vec3f(0.20, 0.20, 0.20), 0, 0.0));
-	spheres.push_back(Sphere(Vec3f( 0.0, 0, -20), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
-	spheres.push_back(Sphere(Vec3f( 5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
-	spheres.push_back(Sphere(Vec3f( 5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
-	spheres.push_back(Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
-	// light
+	// spheres.push_back(Sphere(Vec3f( 0.0, 0, -40), 4, Vec3f(1.00, 0.32, 0.36), 1, 0.5));
+	// spheres.push_back(Sphere(Vec3f( 5.0, -1, -15), 2, Vec3f(0.90, 0.76, 0.46), 1, 0.0));
+	// spheres.push_back(Sphere(Vec3f( 5.0, 0, -25), 3, Vec3f(0.65, 0.77, 0.97), 1, 0.0));
+	// spheres.push_back(Sphere(Vec3f(-5.5, 0, -15), 3, Vec3f(0.90, 0.90, 0.90), 1, 0.0));
+
+    random_spheres(spheres, 100);
+    // light
 	spheres.push_back(Sphere(Vec3f( 0.0, 20, -30), 3, Vec3f(0.00, 0.00, 0.00), 0, 0.0, Vec3f(3)));
 	//*
 	pthread_t threads[NUM_THREADS];
