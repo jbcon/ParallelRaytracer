4cc867a219f8f12d951d41d98a2941a3bacabc0f
John Conover <111jbcon@gmail.com>
2016-05-10 02:15:54 -0400

    Added timing and separated classes into another file

--- raytrace_pthread.cpp
+++ raytrace_pthread.cpp
@@ -6,89 +6,23 @@
 #include <iostream>
 #include <cassert>
 #include <pthread.h>
+#include <ctime>
 
-#if defined __linux__ || defined __APPLE__
-// "Compiled for Linux
-#else
-// Windows doesn't define these values by default, Linux does
-#define M_PI 3.141592653589793
-#define INFINITY 1e8
-#endif
-
-template<typename T>
-class Vec3
-{
-public:
-	T x, y, z;
-	Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-	Vec3(T xx) : x(xx), y(xx), z(xx) {}
-	Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
-	Vec3& normalize()
-	{
-		T nor2 = length2();
-		if (nor2 > 0) {
-			T invNor = 1 / sqrt(nor2);
-			x *= invNor, y *= invNor, z *= invNor;
-		}
-		return *this;
-	}
-	Vec3<T> operator * (const T &f) const { return Vec3<T>(x * f, y * f, z * f); }
-	Vec3<T> operator * (const Vec3<T> &v) const { return Vec3<T>(x * v.x, y * v.y, z * v.z); }
-	T dot(const Vec3<T> &v) const { return x * v.x + y * v.y + z * v.z; }
-	Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
-	Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
-	Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
-	Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
-	Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
-	T length2() const { return x * x + y * y + z * z; }
-	T length() const { return sqrt(length2()); }
-	friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
-	{
-		os << "[" << v.x << " " << v.y << " " << v.z << "]";
-		return os;
-	}
-};
-
-typedef Vec3<float> Vec3f;
-
-class Sphere
-{
-public:
-	Vec3f center; /// position of the sphere
-	float radius, radius2; /// sphere radius and radius^2
-	Vec3f surfaceColor, emissionColor; /// surface color and emission (light)
-	float transparency, reflection; /// surface transparency and reflectivity
-	Sphere(
-		const Vec3f &c,
-		const float &r,
-		const Vec3f &sc,
-		const float &refl = 0,
-		const float &transp = 0,
-		const Vec3f &ec = 0) :
-		center(c), radius(r), radius2(r * r), surfaceColor(sc), emissionColor(ec),
-		transparency(transp), reflection(refl)
-	{ /* empty */ }
-
-	bool intersect(const Vec3f &rayorig, const Vec3f &raydir, float &t0, float &t1) const
-	{
-		Vec3f l = center - rayorig;
-		float tca = l.dot(raydir);
-		if (tca < 0) return false;
-		float d2 = l.dot(l) - tca * tca;
-		if (d2 > radius2) return false;
-		float thc = sqrt(radius2 - d2);
-		t0 = tca - thc;
-		t1 = tca + thc;
-
-		return true;
-	}
-};
+#include "rt_classes.h"
 
 #define MAX_RAY_DEPTH 5
+#define NUM_THREADS 100
+
+unsigned int width = 640, height = 480;
+struct thread_data{
+	int thread_id;
+	std::vector<Sphere> spheres;
+	Vec3f *image;
+};
 
 float mix(const float &a, const float &b, const float &mix)
 {
-return b * mix + a * (1 - mix);
+    return b * mix + a * (1 - mix);
 }
 
 Vec3f trace(
@@ -172,49 +106,11 @@ Vec3f trace(
 	}
 
 	return surfaceColor + sphere->emissionColor;
-} 
-
-void render(const std::vector<Sphere> &spheres)
-{
-	unsigned width = 640, height = 480;
-	Vec3f *image = new Vec3f[width * height], *pixel = image;
-	float invWidth = 1 / float(width), invHeight = 1 / float(height);
-	float fov = 30, aspectratio = width / float(height);
-	float angle = tan(M_PI * 0.5 * fov / 180.);
-	// Trace rays
-	for (unsigned y = 0; y < height; ++y) {
-		for (unsigned x = 0; x < width; ++x, ++pixel) {
-			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
-			float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-			Vec3f raydir(xx, yy, -1);
-			raydir.normalize();
-			*pixel = trace(Vec3f(0), raydir, spheres, 0);
-		}
-	}
-	// Save result to a PPM image (keep these flags if you compile under Windows)
-	std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
-	ofs << "P6\n" << width << " " << height << "\n255\n";
-	for (unsigned i = 0; i < width * height; ++i) {
-		ofs << (unsigned char)(std::min(float(1), image[i].x) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].y) * 255) <<
-			(unsigned char)(std::min(float(1), image[i].z) * 255);
-	}
-	ofs.close();
-	delete [] image;
 }
-
-#define NUM_THREADS 50
-
-struct thread_data{
-	int thread_id;
-	std::vector<Sphere> spheres;
-	Vec3f *image;
-};
 
 void *render_thread(void *threadarg){
 	struct thread_data *data;
 	data = (struct thread_data *) threadarg;
-	unsigned width = 640, height = 480;
 	float invWidth = 1 / float(width), invHeight = 1 / float(height);
 	float fov = 30, aspectratio = width / float(height);
 	float angle = tan(M_PI * 0.5 * fov / 180.);
@@ -237,6 +133,12 @@ void *render_thread(void *threadarg){
 
 int main(int argc, char **argv)
 {
+
+    if (argc == 3){
+        width = atoi(argv[1]);
+        height = atoi(argv[2]);
+    }
+
 	srand48(13);
 	std::vector<Sphere> spheres;
 	// position, radius, surface color, reflectivity, transparency, emission color
@@ -251,9 +153,15 @@ int main(int argc, char **argv)
 	pthread_t threads[NUM_THREADS];
 	struct thread_data td[NUM_THREADS];
 	int rc, i;
-	unsigned width = 640, height = 480;
 	Vec3f *image = new Vec3f[width * height];
-	for(i = 0; i < NUM_THREADS; i++){
+
+    // start timing
+    clock_t start, end;
+    start = clock();
+
+    std::cout << "Starting compute..." << std::endl;
+
+    for(i = 0; i < NUM_THREADS; i++){
 		td[i].thread_id = i;
 		td[i].spheres = spheres;
 		td[i].image = image;
@@ -267,6 +175,9 @@ int main(int argc, char **argv)
 	for (i = 0; i < NUM_THREADS; i++){
 		pthread_join(threads[i], NULL);
 	}
+    end = clock();
+    std::cout << "Compute finished in " << ((float)end-start)/CLOCKS_PER_SEC << " seconds" << std::endl;
+
 	// Save result to a PPM image (keep these flags if you compile under Windows)
 	std::ofstream ofs("./untitled.ppm", std::ios::out | std::ios::binary);
 	ofs << "P6\n" << width << " " << height << "\n255\n";
@@ -280,4 +191,4 @@ int main(int argc, char **argv)
 	//*/
 	//render(spheres);
 	return 0;
-} 
+}
